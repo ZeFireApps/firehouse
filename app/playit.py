99d61ea956360d7e7c0c020159bf3bdce0a41b51
Ramon Iglesias <ramon.d.iglesias@gmail.com>
2014-10-05 18:34:34 -0700

    most simple api

--- app/playit.py
+++ app/playit.py
@@ -1,39 +1,54 @@
 import requests
 import json
+from flask import jsonify
 from app import app
+
+URL = 'http://127.0.0.1:6680/mopidy/rpc'
 
 @app.route('/play')
 def play():
-	url = 'http://127.0.0.1:6680/mopidy/rpc'
 	payload = {'jsonrpc':'2.0', 'id':1,'method':'core.playback.play'}
 	headers = {'content-type':'application.json'}
 
 	r=requests.post(url,data=json.dumps(payload),headers=headers)
-	return r
+	return jsonify(r)
 
 @app.route('/pause')
 def pause():
-	url = 'http://127.0.0.1:6680/mopidy/rpc'
 	payload = {'jsonrpc':'2.0', 'id':1,'method':'core.playback.pause'}
 	headers = {'content-type':'application.json'}
 
 	r=requests.post(url,data=json.dumps(payload),headers=headers)
-	return r
+	return jsonify(r)
 
 @app.route('/stop')
 def stop():
-	url = 'http://127.0.0.1:6680/mopidy/rpc'
 	payload = {'jsonrpc':'2.0', 'id':1,'method':'core.playback.stop'}
 	headers = {'content-type':'application.json'}
 
 	r=requests.post(url,data=json.dumps(payload),headers=headers)
-	return r	
+	return jsonify(r)	
 
 @app.route('/resume')
 def resume():
-	url = 'http://127.0.0.1:6680/mopidy/rpc'
 	payload = {'jsonrpc':'2.0', 'id':1,'method':'core.playback.resume'}
 	headers = {'content-type':'application.json'}
 
 	r=requests.post(url,data=json.dumps(payload),headers=headers)
-	return r
+	return jsonify(r)
+
+@app.route('/add_track')
+def add_track(uri=None):
+	if not uri: uri = request.json['uri']
+	params = {'uri':uri}
+	payload = {'jsonrpc':'2.0', 'id':1,'method':'core.tracklist.add','params':params}
+	headers = {'content-type':'application.json'}
+
+	r=requests.post(url,data=json.dumps(payload),headers=headers)
+	return jsonify(r)
+
+@app.route('/arrived')
+def arrival():
+	add_track(request.json['uri'])
+	play()
+	return jsonify({'message':'Welcome Home: '+str(request.json['username'])})
